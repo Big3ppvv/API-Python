8df153617e490c62dd8a6ea1c2ec5a0af73f9b37
Joaquin <joaquinruizperez2002@gmail.com>
2025-03-21 15:27:06 -0300

    Add Python file

---
+++ main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import requests,json,asyncio
+
+
+app = FastAPI()
+
+# class TrelloUpdateUrl(BaseModel):
+@app.get("/url")
+async def getUrl():
+  id = "TrelloID"
+  url = "https://trello.com/b/EyVgS1IC/api-prueba/boards/cards"
+
+  query = {
+  'key': 'APIKey',
+  'token': 'APIToken'
+  }
+
+  headers = {
+  "Accept": "application/json"
+  } 
+
+  response = requests.request(
+   "get",
+    url,
+    headers=headers,
+    params=query
+  )
+  await response
+  if
+
+
+  
+asyncio.run(getUrl())
+
+  
+
+
+
+
+
+
+
+
